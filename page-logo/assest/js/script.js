e74d448937612133fafd282acffb6753b745c613
Ferdi Yansah <165828731+frdynsh@users.noreply.github.com>
2025-03-11 20:16:38 +0700

    Add files via upload

---
+++ page-logo/assest/js/script.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    grapCursor: true,
+    spaceBetween: 100,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
