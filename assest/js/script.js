906548beb35cfa5fe527a8be3d23c438b8c164ce
frdynsh <frdynsh11@gmail.com>
2025-03-13 10:01:26 +0700

    Menyelesaikan ui desktop

---
+++ assest/js/script.js
@@ -0,0 +1,52 @@
+// PARALAX HERO
+let text = document.querySelector('.text-hero');
+let image = document.querySelector('.image-hero');
+
+let scrollY = 0;
+let targetScrollY = 0;
+let easing = 0.5; 
+
+function smoothScroll() {
+    targetScrollY = window.scrollY;
+    scrollY += (targetScrollY - scrollY) * easing;
+
+    text.style.transform = `translateX(${-scrollY * 2.5}px)`;
+    image.style.transform = `translateY(${scrollY * 2.5}px)`;
+
+    requestAnimationFrame(smoothScroll);
+}
+
+window.addEventListener('scroll', () => {
+    requestAnimationFrame(smoothScroll);
+});
+
+
+
+// CARD HIMTIKA
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    grapCursor: true,
+    spaceBetween: 65,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
+
